2148b895016feb79b37c0206f28a2f9830ee78da
김영현 <https://github.com/dudgus257>
2023-02-28 18:39:17 +0900

    clone-chrome

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(function(){
+    //scroll change download
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 500) {
+            $('.download').addClass('active')
+        }
+        else {
+            $('.download').removeClass('active')
+        }
+    })
+    // sitemap accodian
+    $('.link-item-title').click(function(){
+        // $(this).next().slideToggle()
+        $(this).next().slideDown()
+        $(this).parent().siblings().children('.link-item-content').slideUp()
+
+        $(this).addClass('active')
+        $(this).parent().siblings().children('.link-item-title').removeClass('active')
+    })
+    
+})
